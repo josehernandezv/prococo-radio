62d19a9b6d8a5b3392364009a1d4fa1ffeb8e48d
José Andrés Hernández Vargas <jose.hernandez@doubledigit.com>
2019-02-28 23:38:07 -0600

    minor changes

--- src/components/navigation/Navbar/Navbar.js
+++ src/components/navigation/Navbar/Navbar.js
@@ -5,17 +5,18 @@ import Toolbar from '@material-ui/core/Toolbar';
 // import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import RadioIcon from '@material-ui/icons/Radio';
+import { Link } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const navbar = props => (
     <AppBar position="static" color="default" >
         <Toolbar>
-            <Button size="large">
+            <Button size="large" component={Link} to="/">
                 <RadioIcon className={ classes.logoIcon }/>
                 Prococo Radio
             </Button>
             <div className={ classes.grow }>
-                <Button color="inherit" onClick={ props.onLogout }>Salir</Button>
+                <Button color="inherit" onClick={ props.onLogout } component={Link} to="/">Salir</Button>
             </div>
             
         </Toolbar>

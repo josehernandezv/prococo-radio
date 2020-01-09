ec9706e39b865c738f27e7c51093f7fc3209148e
josehernandezv <jose.andres.art@gmail.com>
2020-01-08 21:54:06 -0600

    removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
-import './App.css';
 import Layout from './components/Layout';
 import Station from './containers/Station';
 import Home from './containers/Home';
-class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<Layout>
-					<Switch>
-						<Route path="/station" exact component={ Station } />
-						<Route path="/" exact component={ Home } />
-						<Redirect to="/" />
-					</Switch>
-				</Layout>
-			</BrowserRouter>
-		);
-	}
-}
+
+const App = () => (
+	<BrowserRouter>
+		<Layout>
+			<Switch>
+				<Route path="/station" exact component={ Station } />
+				<Route path="/" exact component={ Home } />
+				<Redirect to="/" />
+			</Switch>
+		</Layout>
+	</BrowserRouter>
+);
 
 export default App;
